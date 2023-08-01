8f1013770a8e3fbfda53e8dd13c3b405fbfd04f4
khalvai <khalvayozbek@gmail.com>
2023-08-01 17:02:19 +0330

    add : comment module setup

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common';
-import { CommentService } from './comment.service';
-import { CommentResolver } from './comment.resolver';
+import { Module } from "@nestjs/common";
+import { CommentService } from "./comment.service";
+import { CommentResolver } from "./comment.resolver";
+import { MongooseModule } from "@nestjs/mongoose";
+import { CommentSchema } from "./entities/comment.entity";
+import { AuthModule } from "src/auth/auth.module";
+import { RecipeService } from "src/recipe/recipe.service";
+import { RecipeModule } from "src/recipe/recipe.module";
+import { RecipeSchema } from "src/recipe/entities/recipe.entity";
 
 @Module({
-  providers: [CommentResolver, CommentService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: "Comment", schema: CommentSchema },
+      { schema: RecipeSchema, name: "Recipe" },
+    ]),
+    AuthModule,
+    RecipeModule,
+  ],
+  providers: [CommentResolver, CommentService, RecipeService],
 })
 export class CommentModule {}
