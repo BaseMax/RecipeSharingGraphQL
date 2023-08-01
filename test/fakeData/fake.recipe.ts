b3479534a3b03ab8197e4eba3f05cf36bbaf241b
khalvai <khalvayozbek@gmail.com>
2023-08-01 08:59:43 +0330

    test : add get popular test

---
+++ test/fakeData/fake.recipe.ts
@@ -0,0 +1,55 @@
+export const recipes = [
+  {
+    description: "a delicious meal",
+    ingredients: ["meat"],
+    instructions: [
+      {
+        detail: "cook",
+        step: 1,
+      },
+    ],
+    title: "rice and chicken",
+    numberOfLikes: 12,
+    authorId: "64c74934cdd15e463c1e9809",
+  },
+  {
+    description: "a delicious meal",
+    ingredients: ["meat"],
+    instructions: [
+      {
+        detail: "cook",
+        step: 1,
+      },
+    ],
+    title: "pizza",
+    numberOfLikes: 11,
+
+    authorId: "64c74934cdd15e463c1e9809",
+  },
+  {
+    description: "a delicious meal",
+    ingredients: ["meat"],
+    instructions: [
+      {
+        detail: "cook",
+        step: 1,
+      },
+    ],
+    title: "sandwich",
+    numberOfLikes: 10,
+    authorId: "64c74934cdd15e463c1e9809",
+  },
+  {
+    description: "a delicious meal",
+    ingredients: ["meat"],
+    instructions: [
+      {
+        detail: "cook",
+        step: 1,
+      },
+    ],
+    title: "pasta",
+    numberOfLikes: 9,
+    authorId: "64c74934cdd15e463c1e9809",
+  },
+];
