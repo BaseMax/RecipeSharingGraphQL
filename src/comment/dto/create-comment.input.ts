2c08b2edacb844cf0c3b6fa2c61d449ea8259cbd
khalvai <khalvayozbek@gmail.com>
2023-08-01 17:01:51 +0330

    add : comment model

--- src/comment/dto/create-comment.input.ts
+++ src/comment/dto/create-comment.input.ts
@@ -1,7 +1,13 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Int, Field } from "@nestjs/graphql";
+import { IsMongoId, IsString } from "class-validator";
 
 @InputType()
 export class CreateCommentInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsMongoId()
+  recipeId: string;
+
+  @Field()
+  @IsString()
+  content: string;
 }
--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,7 +1,35 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int } from "@nestjs/graphql";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose from "mongoose";
 
+@Schema()
 @ObjectType()
 export class Comment {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  _id: string;
+  @Field()
+  @Prop({ type: String })
+  content: string;
+
+  @Field()
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  })
+  authorId: string;
+
+  @Field()
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Recipe",
+    required: true,
+  })
+  recipeId: string;
+
+  @Field()
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date;
 }
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
