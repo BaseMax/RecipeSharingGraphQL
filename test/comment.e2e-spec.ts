bd3369b9f825e0c4947c34816740d31ecf116427
khalvai <khalvayozbek@gmail.com>
2023-08-02 08:54:17 +0330

    add test : update comment

--- test/comment.e2e-spec.ts
+++ test/comment.e2e-spec.ts
@@ -7,12 +7,14 @@ import { Test } from "@nestjs/testing";
 import { AppModule } from "src/app.module";
 import { getModelToken } from "@nestjs/mongoose";
 import { RecipeDocument } from "src/recipe/interfaces/recIpe.documents";
+import { CommentDocument } from "src/comment/interfaces/comment.document";
 
 describe("Comment", () => {
   let app: INestApplication;
   let commentModel: Model<any>;
   let jwtService: JwtService;
   let configService: ConfigService;
+  let recipeModel: Model<any>;
   let userInDb = {
     email: "test@gmail.com",
     password: "test",
@@ -85,6 +87,35 @@ describe("Comment", () => {
     return response.body.data.createRecipe;
   }
 
+  async function createComment(
+    token: string,
+    recipeId: string
+  ): Promise<CommentDocument> {
+    const createCommentMutation = `mutation CreateComment($createCommentInput: CreateCommentInput!) {
+      createComment(createCommentInput: $createCommentInput) {
+        content
+        authorId
+        recipeId
+        createdAt
+        _id
+      }
+    }`;
+    const response = await request(app.getHttpServer())
+      .post("/graphql")
+      .set("authorization", token)
+      .send({
+        query: createCommentMutation,
+        variables: {
+          createCommentInput: {
+            recipeId: recipeId,
+            content: "thanks it was good recipe",
+          },
+        },
+      });
+
+    return response.body.data.createComment;
+  }
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, ConfigModule.forRoot()],
@@ -96,6 +127,7 @@ describe("Comment", () => {
     configService = new ConfigService();
     jwtService = new JwtService({ secret: configService.get("SECRET_KEY") });
     commentModel = app.get<Model<any>>(getModelToken("Comment"));
+    recipeModel = app.get<Model<any>>(getModelToken("Recipe"));
 
     await commentModel.deleteMany({});
     await app.init();
@@ -205,6 +237,145 @@ describe("Comment", () => {
       expect(response.body.data).toBeDefined();
       expect(authorId).toBe(userId);
       expect(content).toBe("thanks it was good recipe");
+      expect(recipeId).toBe(recipe._id.toString());
+    });
+  });
+
+  describe("update comment", () => {
+    let token: string;
+    let recipe: RecipeDocument;
+    let comment: CommentDocument;
+    const updateCommentMutation = `mutation UpdateComment($updateCommentInput: UpdateCommentInput!) {
+      updateComment(updateCommentInput: $updateCommentInput) {
+        _id
+        content
+        authorId
+        recipeId
+        createdAt
+      }
+    }`;
+    beforeAll(async () => {
+      token = await login();
+      recipe = await createRecipe(token);
+      comment = await createComment(token, recipe._id.toString());
+    });
+
+    afterAll(async () => {
+      await recipeModel.deleteMany();
+      await recipeModel.deleteMany();
+    });
+
+    it("should give authentication error", async () => {
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .send({
+          query: updateCommentMutation,
+          variables: {
+            updateCommentInput: {
+              id: "64c90908dbb8c7ebdaf5aed9",
+              content: null,
+            },
+          },
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toBe(
+        "you must login to get this feather"
+      );
+    });
+
+    it("should give validation error", async () => {
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .set("authorization", token)
+        .send({
+          query: updateCommentMutation,
+          variables: {
+            updateCommentInput: {
+              id: "notMongodbId",
+              content: "new content",
+            },
+          },
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeNull();
+      expect(response.body.errors[0].message).toContain(
+        "id must be a mongodb id"
+      );
+    });
+
+
+    it("should give not found", async () => {
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .set("authorization", token)
+        .send({
+          query: updateCommentMutation,
+          variables: {
+            updateCommentInput: {
+              id: "64c9e67ef2dde04c068f10e3",
+              content: "new content",
+            },
+          },
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeNull();
+      expect(response.body.errors[0].message).toBe(
+        "Comment with this credentials doesn't exist"
+      );
+    });
+
+
+    it("should give not allowed error", async () => {
+      const token = jwtService.sign({
+        sub: "64c74934cdd15e463c1e9802",
+        name: "johnie",
+      });
+
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .set("authorization", token)
+        .send({
+          query: updateCommentMutation,
+          variables: {
+            updateCommentInput: {
+              id: comment._id.toString(),
+              content: "new content",
+            },
+          },
+        });
+
+   
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeNull();
+      expect(response.body.errors[0].message).toBe(
+        "you aren't allowed to modify"
+      );
+    });
+    it("should update comment", async () => {
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .set("authorization", token)
+        .send({
+          query: updateCommentMutation,
+          variables: {
+            updateCommentInput: {
+              id: comment._id.toString(),
+              content: "new content",
+            },
+          },
+        });
+
+      const { _id, content, recipeId } = response.body.data.updateComment;
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeTruthy();
+      expect(_id).toBe(comment._id.toString());
+      expect(content).toBe("new content");
       expect(recipeId).toBe(recipe._id.toString());
     });
   });
